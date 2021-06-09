23c8bbb86b7e9a1a648302a0291d54bb84979f10
HildaKornman <84178454+HildaKornman@users.noreply.github.com>
2021-06-10 00:14:11 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log ('Hello Winc Academy')
+function myName(){
+    let name ="Hilda";
+    document.getElementById ("demo").innerHTML=name;
+    }
+function myFunction7a(){
+    let number =4+4;
+    document.getElementById ("7a").innerHTML=number;
+    }
+function myFunction7b(){
+            let number ='4'+'4';
+            document.getElementById ("7b").innerHTML=number;
+            }
+        function myFunction8a(){
+            let x=8;
+            let y=x+5;
+            document.getElementById ("8a").innerHTML=y;
+            }
+        function myFunction8b(){
+            let b=8;
+            let a=b-18;
+            document.getElementById ("8b").innerHTML=a;
+            }
+        function myFunction8c(){
+            let x=3;
+            let y=8*x;
+            document.getElementById ("8c").innerHTML=y;
+            }       
+        function myFunction8d(){
+            let b=15;
+            let a=b/5;
+            document.getElementById ("8d").innerHTML=a;
+            } 
+        function myFunction8e(){
+            let x=10;
+            let y=x%50;
+            document.getElementById ("8e").innerHTML=y;
+            }
+        function myAge9(){
+            let age=50;
+            document.getElementById ("9").innerHTML=age;
+        }
+        function myAge10(){
+            let age="vijftig";
+            document.getElementById ("10").innerHTML=age;
+        }
